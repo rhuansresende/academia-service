eb5e65cd4b5f77263940d4f9f1d80afe92820400
Rhuan Resende <rhuan.resende@detran.go.gov.br>
2025-10-17 16:47:03 -0300

    feat(): Configuração Projeto

--- src/main/java/br/com/desenvolvimento/logica/academia_service/Startup.java
+++ src/main/java/br/com/desenvolvimento/logica/academia_service/Startup.java
@@ -2,8 +2,10 @@ package br.com.desenvolvimento.logica.academia_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Startup {
 
 	public static void main(String[] args) {
